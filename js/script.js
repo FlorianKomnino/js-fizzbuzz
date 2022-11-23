be90ee8e8181c28655eaa1b5e7514b669148153c
FlorianKomnino <florian.komnino@outlook.it>
2022-11-23 15:17:55 +0100

    added IF to js to print FizzBuzz in console when the number is divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 console.log('Hello World!');
 
 for (let i = 1; i < 101; i++) {
-    console.log(i)
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log('FizzBuzz')
+    } else {
+        console.log(i)
+    }
 }
