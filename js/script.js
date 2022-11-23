97e7f022710e54cacd3a72fa51afc7fcf03a1a6e
FlorianKomnino <florian.komnino@outlook.it>
2022-11-23 16:10:10 +0100

    added IF that adds html element with FizzBuzz written on it every time the number is divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 console.log('Hello World!');
 
+
 //eseguo un ciclo per stampare in console i numeri da 1 a 100
 for (let i = 1; i < 101; i++) {
     //aggiungo un condizionale per stampare cose differenti in casi divvefferenti
@@ -18,5 +19,34 @@ for (let i = 1; i < 101; i++) {
     //ALTRIMENTI RESIDUALE : in tutti gli altri casi faccio stampare solo il numero stesso
     else {
         console.log(i)
+    }
+}
+
+const workingSpace = document.getElementById('workingSpace')
+
+//eseguo un ciclo per stampare in console i numeri da 1 a 100
+for (let i = 1; i < 101; i++) {
+    //aggiungo un condizionale per stampare cose differenti in casi divvefferenti
+    //SE PRIMO CASO : se il numero in questione è divisibile sia per 3 che per 5 faccio stampare FizzBuzz invece del numero
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        workingSpace.innerHTML += `
+        <div class="d-flex justify-content-center custom-align-center custom-bg-green">
+            <div class="custom-text-magenta text-center fs-3">
+                FizzBuzz
+            </div>
+        </div>
+        `;
+    } 
+    //ALTRIMENTI SECONDO CASO : se il numero in questione è divisibile solo per 3 faccio stampare solo Fizz invece del numero
+    else if (i % 3 == 0) {
+
+    } 
+    //ALTRIMENTI TERZO CASO : se il numero in questione è divisibile solo per 5 faccio stampare solo Buzz invece del numero
+    else if (i % 5 == 0) {
+
+    } 
+    //ALTRIMENTI RESIDUALE : in tutti gli altri casi faccio stampare solo il numero stesso
+    else {
+
     }
 }
